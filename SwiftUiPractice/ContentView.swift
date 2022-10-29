a40c7cb0da59a48484593d82b735c532abad51a0
MachonglishproK <kakeru32ito@yahoo.co.jp>
2022-10-29 14:59:28 +0900

    Initial Commit

---
+++ SwiftUiPractice/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SwiftUiPractice
+//
+//  Created by 伊藤走 on 2022/10/29.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ SwiftUiPractice/SwiftUiPracticeApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUiPracticeApp.swift
+//  SwiftUiPractice
+//
+//  Created by 伊藤走 on 2022/10/29.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUiPracticeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
